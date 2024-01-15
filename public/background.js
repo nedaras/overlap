6a6960ced225593930e1a6c8dde1e5020c735d4c
nedaras <nedas.pranskunas@gmail.com>
2024-01-15 15:49:16 +0200

    Saves images and we can use arrows for navigation.

--- public/background.js
+++ public/background.js
@@ -6,8 +6,14 @@ overlay.style.left = '0'
 overlay.style.width = '100vw'
 overlay.style.height = '100vh'
 overlay.style.zIndex = '999999'
-overlay.style.background = 'red'
+overlay.style.background = '#1f1f1f'
 overlay.style.display = 'none'
+overlay.style.placeItems = 'center'
+
+overlay.innerHTML = `<div>Hello World</div>`
+
+let i = 0
+const images = []
 
 overlay.ondrop = ((event) => {
 
@@ -24,7 +30,16 @@ overlay.ondrop = ((event) => {
     img.src = URL.createObjectURL(file)
     img.draggable = false
 
-    overlay.appendChild(img)
+    img.style.maxWidth = '100%'
+    img.style.maxHeight = '100vh'
+
+    i = images.length
+    images.push({
+      name: file.name,
+      element: img
+    })
+
+    overlay.children[0].replaceWith(img)
 
   }
 
@@ -37,8 +52,43 @@ document.body.appendChild(overlay)
 
 window.addEventListener('keydown', (event) => {
 
+  //if (event.key == 'ArrowRight') {
+
+    //i++
+
+    //console.log(i)
+
+    //overlay.children[0].replaceWith(images[i].element)
+
+    //return
+
+  //}
+
+  //if (event.key == 'ArrowLeft') {
+
+    //i--
+
+    //console.log(i)
+
+    //overlay.children[0].replaceWith(images[i].element)
+
+    //return
+
+  //}
+
+  if (images.length && overlay.style.display != 'none') {
+
+    if (event.key == 'ArrowRight') i++
+    if (event.key == 'ArrowLeft') i--
+
+    i = Math.max(Math.min(images.length - 1, i), 0)
+
+    overlay.children[0].replaceWith(images[i].element)
+
+  }
+
   if (event.key.toLocaleLowerCase() != 'g') return
 
-  overlay.style.display = overlay.style.display == 'block' ? 'none' : 'block'
+  overlay.style.display = overlay.style.display == 'none' ? 'grid' : 'none'
 
 })
