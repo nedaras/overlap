0f9754aa4db2a4fa0fa0444d204ef0f0ded03215
nedaras <nedas.pranskunas@gmail.com>
2024-02-03 15:46:18 +0200

    Building a popup. Making the structure.

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,28 @@
+import { createSignal } from 'solid-js'
+
 export default function App() {
 
-  function handleClick() {
-
-    console.log('fuck')
-
-  }
-
+  const [ active, setActive ] = createSignal(true)
+  
   return <>
-    <h1>Overlap</h1>
-    <ul>
-      <li>
-        <button onclick={handleClick} >
-          Press Me
-        </button>
+    <header>
+
+    </header>
+    <ul class='select-none' >
+      <li class='py-1' >
+        <div class='hover:bg-gray-100 px-1' >
+          <div class='w-full text-left pl-4' onclick={() => setActive((active) => !active)} >
+            { active() ? 'Disable' : 'Activate' }
+          </div>
+        </div>
+      </li>
+      <li class='py-1' >
+        <div class='hover:bg-gray-100 px-1' >
+          <div class='flex justify-between gap-8 w-full text-left pl-4' >
+            <div>Shortcut</div>
+            <div>"key"</div>
+          </div>
+        </div>
       </li>
     </ul>
   </>
