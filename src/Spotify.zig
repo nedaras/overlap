e67b3d90c76e108d0acd45a974dc95fd0c17aeb4
Nedas <nedas.pranskunas@gmail.com>
2025-06-03 23:34:25 +0300

    Getting an response from Spotify.

--- src/Spotify.zig
+++ src/Spotify.zig
@@ -2,34 +2,54 @@ const std = @import("std");
 const http = std.http;
 const fmt = std.fmt;
 const json = std.json;
+const crypto = std.crypto;
 const Allocator = std.mem.Allocator;
+const Sha256 = crypto.hash.sha2.Sha256;
+const base64 = std.base64.standard;
 
 client: http.Client,
-client_id: []const u8,
 verifier: []const u8,
+
+client_id: []const u8,
 redirect_uri: []const u8,
 
 const Self = @This();
 
 const Args = struct {
     client_id: []const u8,
-    verifier: []const u8,
     redirect_uri: []const u8,
 };
 
-pub fn init(allocator: Allocator, args: Args) Self {
+pub fn init(allocator: Allocator, args: Args) Allocator.Error!Self {
+    const verifier = try allocator.alloc(u8, 64);
+    randomString(verifier);
+
     return .{
         .client = http.Client{ 
             .allocator = allocator,
         },
+        .verifier = verifier,
         .client_id = args.client_id,
-        .verifier = args.verifier,
         .redirect_uri = args.redirect_uri,
     };
 }
 
-pub fn deinit(spotify: *Self) void {
-    spotify.client.deinit();
+pub fn deinit(self: *Self) void {
+    self.client.allocator.free(self.verifier);
+    self.client.deinit();
+}
+
+pub fn generateOAuthUrl(self: *Self) Allocator.Error![]u8 {
+    var hash: [32]u8 = undefined;
+    var challange: [44]u8 = undefined;
+
+    Sha256.hash(self.verifier, &hash, .{});
+
+    _ = std.base64.url_safe_no_pad.Encoder.encode(&challange, &hash);
+    std.debug.print("{s}\n", .{self.verifier});
+    std.debug.print("{s}\n", .{challange});
+
+    return fmt.allocPrint(self.client.allocator, "https://accounts.spotify.com/authorize?response_type=code&client_id={s}&scope=user-read-private+user-read-email&code_challenge_method=S256&code_challenge={s}&redirect_uri={s}", .{self.client_id, challange, self.redirect_uri});
 }
 
 pub fn retreiveAccessToken(self: *Self, code: []const u8) !void {
@@ -41,18 +61,31 @@ pub fn retreiveAccessToken(self: *Self, code: []const u8) !void {
     });
     defer self.client.allocator.free(payload);
 
+    std.debug.print("{s}\n", .{payload});
+
     var response = std.ArrayList(u8).init(self.client.allocator);
     defer response.deinit();
 
     const status = try self.client.fetch(.{
         .method = .POST,
-        .location = .{ .url = "https://accounts.spotify.com/api/token" },
-        .headers = .{ .content_type = .{ .override = "application/x-www-form-urlencoded" } },
+        //.location = .{ .url = "https://accounts.spotify.com/api/token" },
+        .location = .{ .url = "http://127.0.0.1:3000/api/token" },
+        .headers = .{ 
+            .content_type = .{ .override = "application/x-www-form-urlencoded" }
+        },
         .redirect_behavior = .not_allowed,
         .payload = payload,
         .response_storage = .{ .dynamic = &response },
     });
-    _  = status;
 
-    std.debug.print("{s}\n", .{response.items});
+    std.debug.print("{}\n{s}\n", .{status.status, response.items});
+}
+
+fn randomString(buf: []u8) void {
+    const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    crypto.random.bytes(buf);
+    for (0..buf.len) |i| {
+        buf[i] = chars[buf[i] % chars.len];
+    }
 }
--- src/main.zig
+++ src/main.zig
@@ -1,69 +1,196 @@
 const std = @import("std");
 const builtin = @import("builtin");
-const Spotify = @import("Spotify.zig");
-const http = std.http;
-const crypto = std.crypto;
-const fmt = std.fmt;
 const process = std.process;
+const http = std.http;
+const base64 = std.base64;
+const fmt = std.fmt;
 const net = std.net;
-const Sha256 = crypto.hash.sha2.Sha256;
-const Uri = std.Uri;
-const Allocator = std.mem.Allocator;
-const base64 = std.base64.standard;
+const json = std.json;
 const native_os = builtin.os.tag;
+const Uri = std.Uri;
+const Address = net.Address;
+const Sha256 = std.crypto.hash.sha2.Sha256;
+const Allocator = std.mem.Allocator;
 const assert = std.debug.assert;
 
+const Spotify = struct {
+    allocator: Allocator,
+    http_client: *http.Client,
+
+    client_id: *const [32]u8,
+    code_verifier: *const [64]u8,
+
+    redirect_uri: []const u8,
+
+    pub fn generateOAuthUrl(self: Spotify) Allocator.Error![]u8 {
+        var hash: [32]u8 = undefined;
+        var challange: [43]u8 = undefined;
+
+        Sha256.hash(self.code_verifier, &hash, .{});
+        assert(base64.url_safe_no_pad.Encoder.encode(&challange, &hash).len == 43);
+
+        return try fmt.allocPrint(self.allocator, "https://accounts.spotify.com/authorize?response_type=code&client_id={s}&scope=user-read-private+user-read-email&code_challenge_method=S256&code_challenge={s}&redirect_uri={s}", .{ 
+            self.client_id,
+            challange,
+            self.redirect_uri
+        });
+    }
+
+    pub fn retreiveAccessToken(self: Spotify, code: []const u8) !void {
+        assert(code.len == 298);
+
+        const payload = try fmt.allocPrint(self.allocator, "client_id={s}&grant_type=authorization_code&code={s}&redirect_uri={s}&code_verifier={s}", .{
+            self.client_id,
+            code,
+            self.redirect_uri,
+            self.code_verifier,
+        });
+        defer self.allocator.free(payload);
+
+        var response = std.ArrayList(u8).init(self.allocator);
+        errdefer response.deinit();
+
+        const uri = Uri{
+            .scheme = "https",
+            .host = .{
+                .raw = "accounts.spotify.com",
+            },
+            .path = .{
+                .raw = "/api/token",
+            },
+        };
+
+        var header_buf: [4096]u8 = undefined;
+        var req = try self.http_client.open(.POST, uri, .{
+            .server_header_buffer = &header_buf,
+            .redirect_behavior = .not_allowed,
+            .headers = .{
+                .content_type = .{ .override = "application/x-www-form-urlencoded" }
+            },
+        });
+        defer req.deinit();
+
+        req.transfer_encoding = .{ .content_length = payload.len };
+
+        try req.send();
+        try req.writeAll(payload);
+
+        try req.finish();
+        try req.wait();
+
+        var json_reader = json.reader(self.allocator, req.reader());
+        defer json_reader.deinit();
+
+        const parsed = try json.parseFromTokenSource(struct {
+            access_token: []const u8,
+            token_type: []const u8,
+            expires_in: u32,
+            refresh_token: []const u8,
+            scope: []const u8,
+        }, self.allocator, &json_reader, .{ .ignore_unknown_fields = true });
+        defer parsed.deinit();
+
+        std.debug.print("{s}\n", .{parsed.value.access_token});
+        std.debug.print("{s}\n", .{parsed.value.token_type});
+        std.debug.print("{d}\n", .{parsed.value.expires_in});
+        std.debug.print("{s}\n", .{parsed.value.refresh_token});
+        std.debug.print("{s}\n", .{parsed.value.scope});
+    }
+
+};
+
 pub fn main() !void {
-    var gpa = std.heap.GeneralPurposeAllocator(.{ .verbose_log = false }){};
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
 
+    var gpa2 = std.heap.GeneralPurposeAllocator(.{ .verbose_log = true }){};
+    defer _ = gpa2.deinit();
+
     const allocator = gpa.allocator();
+    const debug_allocator = gpa2.allocator();
 
+    const client_id = "4323d146458c487a9e69c8a6741c5a2b";
+    const redirect_uri = "http%3A%2F%2F127.0.0.1%3A26822%2Foauth%2Fspotify";
 
-    var hash: [32]u8 = undefined;
-    var verifier: [64]u8 = undefined;
-    var challange: [44]u8 = undefined;
+    var code_verifier: [64]u8 = undefined;
+    randomString(&code_verifier);
 
-    randomString(crypto.random, &verifier);
-    Sha256.hash(&verifier, &hash, .{});
+    var client = http.Client{
+        .allocator = allocator,
+    };
+    defer client.deinit();
 
-    assert(base64.Encoder.encode(&challange, &hash).len == 44);
+    const spotify = Spotify{
+        .allocator = debug_allocator,
+        .http_client = &client,
+        .client_id = client_id,
+        .code_verifier = &code_verifier,
+        .redirect_uri = redirect_uri,
+    };
 
-    var spotify = Spotify.init(allocator, .{
-        .client_id = "4323d146458c487a9e69c8a6741c5a2b",
-        .verifier = &verifier,
-        .redirect_uri = "http://127.0.0.1:26822/oauth/spotify",
-    });
-    defer spotify.deinit();
+    {
+        const url = try spotify.generateOAuthUrl();
+        defer debug_allocator.free(url);
 
-    _ = try spotify.retreiveAccessToken("...");
+        try openUrl(allocator, url);
+    }
 
-    const url = try makeAuthUrl(allocator, .{
-        .client_id = "4323d146458c487a9e69c8a6741c5a2b",
-        .challange = &challange,
-        .redirect_uri = "http://127.0.0.1:26822/oauth/spotify",
-    });
-    defer allocator.free(url);
+    const host = Address.initIp4(.{ 127, 0, 0, 1}, 26822);
 
-    try openUrl(allocator, url);
+    var server = try host.listen(.{ .kernel_backlog = 1, .reuse_address = true });
+    defer server.deinit();
 
-    //const host = net.Address.initIp4(.{ 127, 0, 0, 1}, 26822);
+    const connection = try server.accept();
 
-    //var server = try host.listen(.{ .kernel_backlog = 1, .reuse_address = true });
-    //defer server.deinit();
+    var head_buf: [1024]u8 = undefined;
+    var http_server = http.Server.init(connection, &head_buf);
 
-    //const connection = try server.accept();
-    //const stream = connection.stream;
+    var req = try http_server.receiveHead();
 
-    //var buf: [4096]u8 = undefined;
-    //const amt = try stream.read(&buf);
+    const code = req.head.target[20..];
+    assert(code.len == 298);
 
-    //std.debug.print("{s}\n", .{buf[0..amt]});
+    try spotify.retreiveAccessToken(code);
 
-    //try fmt.format(stream.writer(), "HTTP/1.1 200 OK\r\nContent-Length: {d}\r\nConnection: close\r\n\r\n{s}", .{
-        //verifier.len,
-        //verifier,
+    //std.debug.print("{d}\n", .{fba.end_index});
+    //std.debug.print("{s}\n", .{buf});
+
+    //const payload = try fmt.allocPrint(allocator, "client_id={s}&grant_type=authorization_code&code={s}&redirect_uri={s}&code_verifier={s}", .{
+        //client_id,
+        //code,
+        //redirect_uri,
+        //code_verifier,
     //});
+    //defer allocator.free(payload);
+
+    //try req.respond(payload, .{ .keep_alive = false });
+
+    //var response = std.ArrayList(u8).init(allocator);
+    //defer response.deinit();
+
+    //_ = try client.fetch(.{
+        //.method = .POST,
+        //.location = .{ .url = "https://accounts.spotify.com/api/token" },
+        //.headers = .{
+            //.content_type = .{ .override = "application/x-www-form-urlencoded" }
+        //},
+        //.redirect_behavior = .not_allowed,
+        //.payload = payload,
+        //.response_storage = .{
+            //.dynamic = &response,
+        //},
+    //});
+
+    //std.debug.print("{s}\n", .{response.items});
+}
+
+fn randomString(buf: []u8) void {
+    const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    std.crypto.random.bytes(buf);
+    for (0..buf.len) |i| {
+        buf[i] = chars[buf[i] % chars.len];
+    }
 }
 
 fn openUrl(allocator: Allocator, url: []const u8) (process.Child.SpawnError || process.Child.WaitError)!void {
@@ -89,24 +216,4 @@ fn openUrl(allocator: Allocator, url: []const u8) (process.Child.SpawnError || p
     errdefer _ = child.kill() catch {};
 
     _ = try child.wait();
-}
-
-const Args = struct {
-    client_id: []const u8,
-    challange: []const u8,
-    redirect_uri: []const u8,
-};
-
-// there is no need to alloc this, but...
-fn makeAuthUrl(allocator: Allocator, args: Args) error{OutOfMemory}![]u8 {
-    return fmt.allocPrint(allocator, "https://accounts.spotify.com/authorize?response_type=code&client_id={s}&scope=user-read-private%20user-read-email&code_challenge_method=S256&code_challenge={s}&redirect_uri={s}", .{args.client_id, args.challange, args.redirect_uri});
-}
-
-fn randomString(random: std.Random, buf: []u8) void {
-    const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-
-    random.bytes(buf);
-    for (0..buf.len) |i| {
-        buf[i] = chars[buf[i] % chars.len];
-    }
 }
