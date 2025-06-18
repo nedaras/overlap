289d1104e6c8bf29bba52eb27b72401a4d3b9078
Nedas <nedas.pranskunas@gmail.com>
2025-06-18 21:48:03 +0300

    Added fat font.

--- src/main.zig
+++ src/main.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const fat = @import("fat.zig");
 const hook = @import("hook.zig");
 const gui = hook.gui;
 
@@ -11,16 +12,27 @@ const allocator = da.allocator();
 
 var img: hook.Image = undefined;
 
-const fw = 10483;
-const fh = 27;
+var fw: u16 = undefined;
+var fh: u16 = undefined;
 
 // todo: add err handling for init
 fn init() void {
+    const font = @embedFile("gui/font.fat");
+    var fbs = std.io.fixedBufferStream(font);
+
+    const head = fbs.reader().readStruct(fat.Header) catch unreachable;
+    for (0..head.glyphs_len) |_| {
+        _ = fbs.reader().readStruct(fat.Glyph) catch unreachable;
+    }
+
+    fw = head.tex_width;
+    fh = head.tex_height;
+
     img = hook.loadImage(allocator, .{
         .width = fw,
         .height = fh,
         .format = .R,
-        .data = @embedFile("gui/font.bmp"),
+        .data = font[fbs.pos..],
     }) catch unreachable;
 }
 
@@ -31,11 +43,14 @@ fn cleanup() void {
 var x: f32 = 0.0;
 
 fn frame() !void {
-    const slide = @mod(x, fw);
+    const ffw: f32 = @floatFromInt(fw);
+    const ffh: f32 = @floatFromInt(fh);
+
+    const slide = @mod(x, ffw);
     defer x += 0.5;
 
     gui.rect(.{ 100.0, 100.0 }, .{ 500.0, 500.0 }, 0x0F191EFF);
-    gui.image(.{ slide, 300.0 }, .{ slide + fw, 300.0 + fh }, img);
+    gui.image(.{ slide, 300.0 }, .{ slide + ffw, 300.0 + ffh }, img);
 
     // such a simple function no?
     gui.text(.{ 200.0, 200.0 }, "Helo");
