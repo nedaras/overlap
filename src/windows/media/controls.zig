d432d78766e2439aa9871bce182d0b873712714d
nedaras <nedas.pranskunas@gmail.com>
2025-09-27 15:41:21 +0300

    Now program atleast compiles.

--- src/windows/media/controls.zig
+++ src/windows/media/controls.zig
@@ -100,8 +100,8 @@ pub const IGlobalSystemMediaTransportControlsSessionPlaybackInfo = extern struct
         IUnknown.Release(@ptrCast(self));
     }
 
-    pub fn get_PlaybackStatus(self: *IGlobalSystemMediaTransportControlsSessionPlaybackInfo) MediaPlaybackStatus  {
-        const FnType = fn (*IGlobalSystemMediaTransportControlsSessionPlaybackInfo, *c_int) callconv(.winapi) HRESULT;
+    pub fn get_PlaybackStatus(self: *IGlobalSystemMediaTransportControlsSessionPlaybackInfo) MediaPlaybackStatus {
+        const FnType = fn (*IGlobalSystemMediaTransportControlsSessionPlaybackInfo, *MediaPlaybackStatus) callconv(.winapi) HRESULT;
         const get_playback_status: *const FnType = @ptrCast(self.vtable[7]);
 
         var value: MediaPlaybackStatus = undefined;
