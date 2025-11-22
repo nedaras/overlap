d0c082488923abaf9c3126b9999ab849c05471b0
Nedas Pranskunas <nedas.pranskunas@gmail.com>
2025-11-22 17:36:26 +0200

    Doing a rewrite again...

--- src/libmain2.zig
+++ src/libmain2.zig
@@ -2,160 +2,26 @@ const std = @import("std");
 const windows = @import("windows.zig");
 const detours = @import("detours.zig");
 const Hooks = @import("Hooks.zig");
+const Thread = std.Thread;
 
-const mem = std.mem;
-
-var hooks: Hooks = .init;
-
-var load_library_a: ?*@TypeOf(LoadLibraryA) = null;
-var load_library_w: ?*@TypeOf(LoadLibraryW) = null;
-
-fn attach() void {
-}
+var main_proc: ?Thread = null;
+var detach_event: Thread.ResetEvent = .{};
 
 pub export fn __overlap_hook_proc(code: c_int, wParam: windows.WPARAM, lParam: windows.LPARAM) callconv(.winapi) windows.LRESULT {
     return windows.user32.CallNextHookEx(null, code, wParam, lParam);
 }
 
 pub export fn DllMain(hinstDLL: windows.HINSTANCE, fdwReason: windows.DWORD, lpvReserved: windows.LPVOID) callconv(.winapi) windows.BOOL {
-    _ = hinstDLL;
     _ = lpvReserved;
 
     switch (fdwReason) {
         windows.DLL_PROCESS_ATTACH => {
-            //const kernel32 = windows.GetModuleHandle("kernel32.dll") orelse {
-                //std.log.err("module 'kernel32.dll' is not loaded.", .{});
-                //return windows.FALSE;
-            //};
-
-            //load_library_a = @ptrCast(windows.GetProcAddress(kernel32, "LoadLibraryA") catch return windows.FALSE);
-            //load_library_w = @ptrCast(windows.GetProcAddress(kernel32, "LoadLibraryW") catch return windows.FALSE);
-
-            //detours.attach(LoadLibraryA, &load_library_a.?) catch |err| {
-                //std.log.err("failed to hook 'LoadLibraryA': {}`", .{err});
-                //load_library_a = null;
-                //return windows.FALSE;
-            //};
-            //std.log.info("hooked 'LoadLibraryA'", .{});
-
-            //detours.attach(LoadLibraryW, &load_library_w.?) catch |err| {
-                //std.log.err("failed to hook 'LoadLibraryW': {}`", .{err});
-                //load_library_w = null;
-                //return windows.FALSE;
-            //};
-            //std.log.info("hooked 'LoadLibraryA'", .{});
-
-            if (windows.GetModuleHandle("d3d11.dll")) |d3d11_lib| {
-                if (windows.GetModuleHandle("dxgi.dll") == null) {
-                    std.log.info("dxgi not loaded!", .{});
-                    return windows.FALSE;
-                }
-
-
-                std.log.info("d3d11 is already loaded!", .{});
-
-                const d3d11 = windows.d3d11;
-                const dxgi = windows.dxgi;
-                const d3dcommon = windows.d3dcommon;
-
-                const window = windows.CreateWindowEx(
-                    0,
-                    "STATIC",
-                    "Overlap DXGI Window",
-                    windows.WS_OVERLAPPEDWINDOW,
-                    windows.CW_USEDEFAULT,
-                    windows.CW_USEDEFAULT,
-                    640,
-                    480,
-                    null,
-                    null,
-                    null,
-                    null,
-                ) catch |err| {
-                    std.log.err("failed creating a window: {}", .{err});
-                    return windows.FALSE;
-                };
-                defer windows.DestroyWindow(window);
-
-                const D3D11CreateDeviceAndSwapChain = *const @TypeOf(d3d11.D3D11CreateDeviceAndSwapChain);
-                const d3d11_create_device_and_swap_chain: D3D11CreateDeviceAndSwapChain = @ptrCast(windows.GetProcAddress(
-                        d3d11_lib,
-                        "D3D11CreateDeviceAndSwapChain",
-                ) catch |err| {
-                    std.log.err("failed to get address for 'D3D11CreateDeviceAndSwapChain': {}", .{err});
-                    return windows.FALSE;
-                });
-
-                var sd = std.mem.zeroes(dxgi.DXGI_SWAP_CHAIN_DESC);
-                sd.BufferCount = 1;
-                sd.BufferDesc.Format = dxgi.DXGI_FORMAT_R8G8B8A8_UNORM;
-                sd.OutputWindow = window;
-                sd.SampleDesc.Count = 1;
-                sd.Windowed = windows.TRUE;
-                sd.SwapEffect = dxgi.DXGI_SWAP_EFFECT_DISCARD;
-
-                var swap_chain: *dxgi.IDXGISwapChain = undefined;
-
-                var device: *d3d11.ID3D11Device = undefined;
-                var device_context: *d3d11.ID3D11DeviceContext = undefined;
-
-                const feature_levels = [_]d3dcommon.D3D_FEATURE_LEVEL{
-                    d3dcommon.D3D_FEATURE_LEVEL_11_0,
-                    d3dcommon.D3D_FEATURE_LEVEL_10_1,
-                    d3dcommon.D3D_FEATURE_LEVEL_10_0,
-                };
-
-                const hr = d3d11_create_device_and_swap_chain(
-                    null,
-                    d3dcommon.D3D_DRIVER_TYPE_HARDWARE,
-                    null,
-                    0,
-                    &feature_levels,
-                    feature_levels.len,
-                    d3d11.D3D11_SDK_VERSION,
-                    &sd,
-                    &swap_chain,
-                    &device,
-                    null,
-                    &device_context,
-                );
-
-                switch (d3d11.D3D11_ERROR_CODE(hr)) {
-                    .S_OK => {},
-                    else => |err| {
-                        std.log.err("failed to create device and swapchain: {}", .{err});
-                        return windows.FALSE;
-                    },
-                }
-
-                defer swap_chain.Release();
-                defer device.Release();
-                defer device_context.Release();
-
-                std.log.info("all good", .{});
-
-                //hooks.attach(.{ .d3d11 = d3d11 }) catch |err| {
-                    //std.log.err("could not hook d3d11: {}", .{err});
-                    //return windows.FALSE;
-                //};
-
-                //std.log.info("hooked d3d11", .{});
-            }
+            windows.DisableThreadLibraryCalls(@ptrCast(hinstDLL)) catch {};
+            main_proc = Thread.spawn(.{}, entry, .{}) catch return windows.FALSE;
         },
-        windows.DLL_PROCESS_DETACH => {
-            // !!! if d3d11 is unloaded this will probably fail rly badly
-            // hook FreeLibrary i guess idk
-            hooks.deinit();
-
-            if (load_library_a) |*func| {
-                detours.detach(LoadLibraryA, func) catch {};
-                load_library_a = null;
-            }
-
-            if (load_library_w) |*func| {
-                detours.detach(LoadLibraryW, func) catch {};
-                load_library_w = null;
-            }
+        windows.DLL_PROCESS_DETACH => if (main_proc) |thread| {
+            detach_event.reset();
+            thread.join();
         },
         else => {},
     }
@@ -163,37 +29,14 @@ pub export fn DllMain(hinstDLL: windows.HINSTANCE, fdwReason: windows.DWORD, lpv
     return windows.TRUE;
 }
 
-fn LoadLibraryA(lpLibFileName: windows.LPCSTR) callconv(.winapi) ?windows.HMODULE {
-    const library = load_library_a.?(lpLibFileName) orelse return null;
 
-    if (mem.eql(u8, mem.span(lpLibFileName), "d3d11.dll")) blk: {
-        hooks.attach(.{ .d3d11 = library }) catch |err| {
-            std.log.err("could not hook d3d11: {}", .{err});
-            break :blk;
-        };
-
-        std.log.info("hooked d3d11", .{});
-    }
-
-    return library;
+fn entry() void {
+    std.log.info("DLL_PROCESS_ATTACH", .{});
+    detach_event.wait();
+    std.log.info("DLL_PROCESS_DETACH", .{});
 }
 
-fn LoadLibraryW(lpLibFileName: windows.LPCWSTR) callconv(.winapi) ?windows.HMODULE {
-    const library = load_library_w.?(lpLibFileName) orelse return null;
-
-    if (mem.eql(u16, mem.span(lpLibFileName), std.unicode.wtf8ToWtf16LeStringLiteral("d3d11.dll"))) blk: {
-        hooks.attach(.{ .d3d11 = library }) catch |err| {
-            std.log.err("could not hook d3d11: {}", .{err});
-            break :blk;
-        };
-
-        std.log.info("hooked d3d11", .{});
-    }
-
-    return library;
-}
-
-pub fn logFn(
+fn logFn(
     comptime message_level: std.log.Level,
     comptime scope: @Type(.enum_literal),
     comptime format: []const u8,
